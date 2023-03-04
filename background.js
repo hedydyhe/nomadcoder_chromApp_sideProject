4bf762edbd6972fa382818e32ff18176211ea67d
hedydyhe <125260101+hedydyhe@users.noreply.github.com>
2023-03-04 21:08:56 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // 랜덤한 img.src 
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 const randomImg = images[Math.floor(Math.random() * images.length)];
-const imgSrc = `img/${randomImg}`;
+const imgSrc = `${randomImg}`;
 
 document.body.style.backgroundImage = `url(${imgSrc})`;
