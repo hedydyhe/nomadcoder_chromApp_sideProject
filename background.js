458290ccf4c9580d41d67676719c534e1d054d45
hedydyhe <125260101+hedydyhe@users.noreply.github.com>
2023-03-04 23:47:13 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 const randomImg = images[Math.floor(Math.random() * images.length)];
-const imgSrc = `img/${randomImg}`;
+const imgSrc = `./img/${randomImg}`;
 
 document.body.style.backgroundImage = `url(${imgSrc})`;
