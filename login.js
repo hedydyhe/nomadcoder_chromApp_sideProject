10f9f425b194c1d42a1728405872ddae3b38f99f
hedydyhe <125260101+hedydyhe@users.noreply.github.com>
2023-03-04 21:01:32 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 const usernameForm = document.querySelector("#username");
 const usernameInput = document.querySelector("#username input:first-child");
 const greeting = document.querySelector("#greeting");
+const todosForm = document.querySelector("#todos");
 
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
@@ -10,6 +11,8 @@ function submitUsername(event) {
   const username = usernameInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   showGreeting(username);
+  todosForm.classList.remove(HIDDEN_CLASS);
+
 }
 
 function showGreeting(username) {
@@ -20,6 +23,7 @@ function showGreeting(username) {
 const getUsername = localStorage.getItem(USERNAME_KEY);
 
 if (getUsername === null) {
+  todosForm.classList.add(HIDDEN_CLASS);
   usernameForm.classList.remove(HIDDEN_CLASS);
   usernameForm.addEventListener("submit", submitUsername);
 } else {
